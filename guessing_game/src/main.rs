7a2facfcdc934282c462fa5e4184da553e3ea017
anthonyrende <rende718@gmail.com>
2023-05-21 23:12:39 -0400

    Add comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,11 +11,20 @@ fn main() {
 
         let mut guess = String::new();
 
+        // std::io::Stdin a type that represents a handle to the standard input for your terminal.
         io::stdin()
+            // read_line() takes whatever the user types into standard input and places that into a string, so it takes that string as an argument.
             .read_line(&mut guess)
+            // read_line() returns a value of io::Result, which is an enum that has the variants Ok or Err.
+            // The Ok value means all went well, and the Err value means there was some kind of problem.
+            // The expect() method is one of the ways you can choose what to do with a Result.
             .expect("Failed to read line");
 
+        // shadowing the previous value of guess with a new one
+        // trim() eliminates whitespace and newlines
+        // parse() returns a Result type, which is an enum with variants Ok and Err
         let guess: u32 = match guess.trim().parse() {
+            // The underscore, _, is a catchall value; in this example, we’re saying we want to match all Err values, no matter what information they have inside them.
             Ok(num) => num,
             Err(_) => {
                 println!("Please type a number! \n");
@@ -23,6 +32,7 @@ fn main() {
             }
         };
 
+        // std::cmp::Ordering is an enum with variants Less, Greater, and Equal
         match guess.cmp(&secret_number) {
             std::cmp::Ordering::Less => println!("Too small!"),
             std::cmp::Ordering::Greater => println!("Too big!"),
