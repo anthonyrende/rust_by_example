85d3b4ddca7a8d8e4fc5cdddc319d976bba3953c
anthonyrende <rende718@gmail.com>
2023-05-28 23:28:53 -0400

    Matching wiht Option<T>

--- enum-control-flow/src/main.rs
+++ enum-control-flow/src/main.rs
@@ -18,6 +18,11 @@ fn main() {
 
     value_in_cents(new_coin);
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("Plus one returns {:?}", six);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -32,5 +37,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("state quarter from {:?}", state);
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
