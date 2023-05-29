8e6fbdf5b1fd82c0c8d1c187b2518ad70c978e0a
anthonyrende <rende718@gmail.com>
2023-05-28 23:24:29 -0400

    Bind enum to a UsState value via match arm

--- enum-control-flow/src/main.rs
+++ enum-control-flow/src/main.rs
@@ -3,13 +3,21 @@ enum Coin {
     Penny,
     Nickle,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
 fn main() {
     let new_coin = Coin::Penny;
 
     value_in_cents(new_coin);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -20,6 +28,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickle => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}", state);
+            25
+        }
     }
 }
