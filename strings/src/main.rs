7aea74d4f8f37b0161b21042f9e04b4b0af86b63
anthonyrende <rende718@gmail.com>
2023-05-30 22:51:39 -0400

    Concatenation with the + Operator or the format! Macro

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -30,4 +30,22 @@ fn main() {
     // The push method takes a single character as a parameter and adds it to the String. Listing 8-17 adds the letter “l” to a String using the push method.
     let mut s = String::from("lo");
     s.push('l');
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    /*
+    The reason s1 is no longer valid after the addition, and the reason we used a reference to s2,
+        has to do with the signature of the method that’s called when we use the + operator.
+    The + operator uses the add method, whose signature looks something like this:
+        fn add(self, s: &str) -> String {
+     */
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+    println!("s3: {}", s3);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
 }
