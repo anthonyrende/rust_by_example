958aa11a72ccbbf245f4059ba1e0163a3d1b6874
anthonyrende <rende718@gmail.com>
2023-06-03 12:39:51 -0400

    updating hash maps

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -20,4 +20,32 @@ fn main() {
     for (key, value) in &scores {
         println!("Key {key}: Value: {value}");
     }
+
+    // Updating a Hash Map
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("map: {:?}", map);
+
+    // Adding a Key and Value Only If a Key Isn’t Present
+    scores.entry(String::from("Yellow")).or_insert(50);
+
+    println!("Scores: {:?}", scores);
+
+    // Updating a Value Based on the Old Value
+    let text = "Hello world wonderful world";
+
+    let mut new_map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        // The or_insert method returns a mutable reference (&mut V)
+        // we store that mutable reference in the count variable
+        let count = new_map.entry(word).or_insert(0);
+        // in order to assign to that value, we must first dereference count
+        *count += 1;
+    } 
+    println!("new map: {:?}", new_map);
 }
