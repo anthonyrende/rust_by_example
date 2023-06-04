a3dbf794ef7b3c4536a5b2437832013dd7567629
anthonyrende <rende718@gmail.com>
2023-06-03 22:40:34 -0400

    get median of vector of integers

--- hash-map-exercises/src/main.rs
+++ hash-map-exercises/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut data = vec![1,2,3,4,5,6,7];
     
     println!("Mean: {}", mean(&data));
+    println!("Median: {}", median(&mut data));
 }
 
 fn mean(numbers: &Vec<i32>) -> i32 {
@@ -13,4 +14,25 @@ fn mean(numbers: &Vec<i32>) -> i32 {
     }
     sum / numbers.len() as i32
 
+}
+/*
+Given a list of integers, use a vector and return the median 
+(when sorted, the value in the middle position) and mode 
+(the value that occurs most often; a hash map will be helpful here) 
+of the list.
+ */
+
+fn median(numbers: &mut Vec<i32>) -> i32 {
+    numbers.sort();
+
+    let middle = numbers.len() / 2;
+    let median: i32 = 0;
+    if numbers.len() % 2 == 0 {
+        mean(&vec!(numbers[middle], numbers[middle - 1])) as i32
+    }
+    else {
+        numbers[middle]
+    }
+    
+    
 }
