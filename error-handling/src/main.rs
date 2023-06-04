84feffe2b436fa4355ddeb2b5e938f14724fe4b8
anthonyrende <rende718@gmail.com>
2023-06-04 16:43:32 -0400

    using unwrap and expect as shortcuts for panic on error

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -16,4 +16,17 @@ fn main() {
             }
         }
     };
+
+    // Shortcuts for Panic on Error: unwrap and expect
+
+    // If the Result value is the Ok variant, unwrap will return the value inside the Ok. 
+    // If the Result is the Err variant, unwrap will call the panic! macro 
+    let greeting_file = File::open("hello.txt").unwrap();
+
+    // Similarly, the expect method lets us also choose the panic! error message. 
+    // Using expect instead of unwrap and providing good error messages can convey your intent
+    //  and make tracking down the source of a panic easier
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
+
 }
