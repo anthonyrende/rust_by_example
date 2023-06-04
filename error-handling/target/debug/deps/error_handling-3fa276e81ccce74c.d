43ff38a52b22804748a3d9c58d04d3d3c2ade367
anthonyrende <rende718@gmail.com>
2023-06-04 11:44:02 -0400

    handling recoverable errors with Result enum

---
+++ error-handling/target/debug/deps/error_handling-3fa276e81ccce74c.d
@@ -0,0 +1,5 @@
+/home/ant/code/rust/rust-by-exmaple/error-handling/target/debug/deps/error_handling-3fa276e81ccce74c: src/main.rs
+
+/home/ant/code/rust/rust-by-exmaple/error-handling/target/debug/deps/error_handling-3fa276e81ccce74c.d: src/main.rs
+
+src/main.rs:
---
+++ error-handling/target/debug/error-handling.d
@@ -0,0 +1,1 @@
+/home/ant/code/rust/rust-by-exmaple/error-handling/target/debug/error-handling: /home/ant/code/rust/rust-by-exmaple/error-handling/src/main.rs
