411aa0594d5c79e3873459ab5d182f20f02ae188
anthonyrende <rende718@gmail.com>
2023-06-04 23:27:18 -0400

    refractor 'largest' function with generic types

--- generic-types/src/main.rs
+++ generic-types/src/main.rs
@@ -1,16 +1,10 @@
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
+/*
+largest is generic over some type T. 
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+This function has one parameter named list, which is a slice of values of type T. 
+The largest function will return a reference to a value of the same type T.
+ */
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -25,11 +19,11 @@ fn largest_char(list: &[char]) -> &char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
