289a20dc0bd464856ed034199c32204b2d6d0279
anthonyrende <rende718@gmail.com>
2023-06-22 22:24:52 -0400

    implementing the Summary trait on Newsarticle and Tweet types

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -11,6 +11,33 @@ pub trait Summary {
     // Each type implementing this trait must provide its own custom behavior 
     fn summarize(&self) -> String;
 }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+/*
+    Implementing a trait on a type is similar to implementing regular methods. The difference is that after impl, we put the trait name we want to implement, then use the for keyword, and then specify the name of the type we want to implement the trait for
+ */
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
 
 fn main() {
     println!("Hello, world!");
