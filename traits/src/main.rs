3a802d8cd0388ea9f3ccef15a6bcc3b80e9dff2d
anthonyrende <rende718@gmail.com>
2023-06-22 22:22:28 -0400

    note on traits

---
+++ traits/src/main.rs
@@ -0,0 +1,17 @@
+/*
+    A trait defines functionality a particular type has and can share with other types. 
+    We can use traits to define shared behavior in an abstract way. 
+    We can use trait bounds to specify that a generic type can be any type that has certain behavior.
+
+    Note: Traits are similar to a feature often called interfaces in other languages, 
+    although with some differences.
+*/
+
+pub trait Summary {
+    // Each type implementing this trait must provide its own custom behavior 
+    fn summarize(&self) -> String;
+}
+
+fn main() {
+    println!("Hello, world!");
+}
